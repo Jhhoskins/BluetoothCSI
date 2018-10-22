57817d0217e0d911a3947a245345cf82fdd066fc
Jesse Hogan Hoskins <hoskin24@egr.msu.edu>
2018-10-22 17:20:36 -0400

    initial commit

---
+++ cse824main.m
@@ -0,0 +1,24 @@
+% CSE 824 Project Main
+% Main Driver File
+% Jesse Hoskins & AJ Saclayan
+% Started on 10/22/18
+
+%% import data
+
+%import bluetooth rssi
+
+%import estimated csi info
+
+%% Initial data plots
+
+%% Generate signal strength model
+
+%filter
+
+%denoise
+
+%dimensionality reduction?
+
+%% Keystroke extraction
+
+%% Print Results

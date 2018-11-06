261c946d41f1817350f43b18fb1a7e4181cd0c4d
Jesse Hogan Hoskins <hoskin24@egr.msu.edu>
2018-11-06 15:36:47 -0500

    cleaned up preprocess and added comments

--- cse824_DataPreprocess.m
+++ cse824_DataPreprocess.m
@@ -4,15 +4,24 @@ rssi_data=csvread(filename);
 std_rssi = std(rssi_data);
 mean_rssi = mean(rssi_data);
 
+%eliminate outliers
 rssi_data(rssi_data<(mean_rssi-abs(2*std_rssi)))=[];
 rssi_data(rssi_data>(mean_rssi+abs(2*std_rssi)))=[];
+%normalize around the mean
 rssi_data=rssi_data./abs(mean_rssi);
-mean(rssi_data)
+% %apply a first order differential
+% rssi_data=diff(rssi_data);
+
+%%Other nromalization fi desired
 %rssi_data=rssi_data+abs(mean_rssi);
 %rssi_data=rssi_data+abs(min(rssi_data));
-rssi_data=rssi_data./abs(min(rssi_data));
-rssi_data=rssi_data+1;
+%rssi_data=rssi_data./abs(min(rssi_data));
+%rssi_data=rssi_data+1;
 %rssi_data=rssi_data./abs(mean_rssi);
+
+%generate real time, assuming 1 reading/sec
+time = linspace(0, length(rssi_data), length(rssi_data));
+%generate fake time
 time = linspace(0, 50, length(rssi_data));
 
 figure

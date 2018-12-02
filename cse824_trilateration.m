3818cc829fcc4d4c8e897f21958703dacdea1003
AJ Saclayan <ajsaclayan@gmail.com>
2018-12-01 20:17:12 -0500

    attempt to get trilateration working

---
+++ cse824_trilateration.m
@@ -0,0 +1,48 @@
+function [ s1 ] = cse824_trilateration(dist)
+s1 = [];    
+    %x and y coords of the three beacons in ft.
+    %ble1(jbl)
+    ble1 = [2.875 0 0]';
+    %ble2(minijacket)
+    ble2 = [-2.125 -2.20833 0]';
+    %ble3(tao)
+    ble3 = [-2.125 2.20833 0]';
+    
+    for i=length(dist.Distance1)
+        da = dist.Distance1(i,1)
+        db = dist.Distance2(i,1)
+        dc = dist.Distance3(i,1)
+
+        P = [ble1 ble2 ble3];
+        S = [da db dc];
+
+        A = []; b = [];
+        
+        %setup alpha and beta coefficients
+        for beaconIdx=1:3
+            x = P(1,beaconIdx); y = P(2,beaconIdx); z = P(3,beaconIdx);
+            s = S(beaconIdx);
+            A = [A ; 1 -2*x  -2*y  -2*z]; 
+            b = [b ; s^2-x^2-y^2-z^2 ];
+        end
+
+        warning off;
+        Xp= A\b; 
+
+        xp = Xp(2:4,:);
+        Z = null(A,'r');
+        z = Z(2:4,:);
+
+        %setup polynomial coefficients
+        a2 = z(1)^2 + z(2)^2 + z(3)^2 ;
+        a1 = 2*(z(1)*xp(1) + z(2)*xp(2) + z(3)*xp(3))-Z(1);
+        a0 = xp(1)^2 +  xp(2)^2+  xp(3)^2-Xp(1);
+        p = [a2 a1 a0];
+        t = roots(p);
+
+        %store s1
+        s1 = [s ; Xp + t(1)*Z];
+
+    end
+
+end
